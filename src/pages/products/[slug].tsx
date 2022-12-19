135e0da0d6454551c2b8ed5c44aa5b34ae6d8875
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-19 12:35:31 -0300

    feat(product): making some changes on product page to fit new design

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,142 +1,54 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
-import { ShoppingCart, Star } from 'phosphor-react'
-import { MouseEvent } from 'react'
-import Stripe from 'stripe'
-import { Button } from '../../components/Button'
-import { IProduct, useCart } from '../../contexts/CartContext'
-import { stripe } from '../../services/stripe'
+
+import { IProduct } from '../../contexts/CartContext'
+
+import imageUrl from '../../assets/sneaker1.png'
 
 type ProductProps = NextPage & {
   product: IProduct
 }
 
-export default function Product({ product }: ProductProps) {
-  const { addItemToCart, checkIfItemAlreadyExists } = useCart()
-  const { isFallback } = useRouter()
-
-  function handleAddToCart(
-    e: MouseEvent<HTMLButtonElement>,
-    product: IProduct
-  ) {
-    e.preventDefault()
-    addItemToCart(product)
-  }
-
-  if (isFallback) return <p>carregando</p>
-
-  const productAlreadyInCart = checkIfItemAlreadyExists(product.id)
-
+export default function Product({}: ProductProps) {
   return (
     <>
       <Head>
-        <title>{product.name} | Sneakers Shop</title>
+        <title> produto | Sneakers Shop</title>
       </Head>
       <main className="main-height">
         <div className="container flex flex-col items-center gap-16 lg:flex-row lg:h-full pb-14">
           <div className="relative rounded-lg overflow-hidden object-center w-[26rem] min-h-[25rem] object-cover">
-            <Image
-              src={product.imageUrl}
-              alt={product.name}
-              fill
-              quality={100}
-              placeholder="blur"
-              blurDataURL={product.imageUrl}
-            />
+            <Image src={imageUrl} alt="" fill quality={100} />
           </div>
 
           <div className="flex-1">
-            <h1 className="my-5 text-3xl font-bold text-gray-900 md:text-5xl">
-              {product.name}
+            <h1 className="my-5 text-3xl font-bold text-gray-900 md:text-4xl">
+              Nike Blazer Young 2022
             </h1>
 
-            <div className="flex items-center gap-2">
-              <Star size={24} weight="fill" className="text-yellow-500" />
-              <Star size={24} weight="fill" className="text-yellow-500" />
-              <Star size={24} weight="fill" className="text-yellow-500" />
-              <Star size={24} weight="fill" className="text-yellow-500" />
-              <Star size={24} weight="fill" className="text-yellow-500" />
-
-              <span className="text-xs font-bold">(1 avaliação)</span>
-            </div>
-
-            <p className="mt-6 mb-6 text-xl leading-relaxed text-gray-900 md:text-2xl">
-              {product.description}
+            <p className="mt-6 mb-6 text-base leading-relaxed text-gray-900 md:text-xl">
+              Esses tênis de perfil baixo são o seu companheiro de roupa casual
+              perfeito. Apresentando uma sola exterior de borracha durável. Eles
+              irão suportar tudo o que o tempo pode oferecer.
             </p>
 
-            <p className="text-2xl font-bold text-gray-900 md:text-4xl">
-              {product.promotionPrice}
+            <p className="text-xl font-bold text-gray-900 md:text-2xl">
+              R$ 199,90
             </p>
 
-            <p className="mt-2 text-lg font-bold text-gray-700 line-through">
-              {product.defaultPrice}
+            <p className="mt-2 text-base font-bold text-gray-900 line-through opacity-50">
+              R$ 250,00
             </p>
 
             <div className="flex gap-6 mt-6">
-              <Button
-                type="button"
-                disabled={checkIfItemAlreadyExists(product.id)}
-                onClick={(e) => handleAddToCart(e, product)}
-              >
-                {productAlreadyInCart ? (
-                  'Produto já está no carrinho'
-                ) : (
-                  <>
-                    Adicionar ao carrinho <ShoppingCart size={24} />
-                  </>
-                )}
-              </Button>
+              <button className="h-12 px-4 py-2 text-sm font-bold leading-5 text-white uppercase bg-black justify-self-center w-max">
+                Comprar agora
+              </button>
             </div>
           </div>
         </div>
       </main>
     </>
   )
-}
-
-export const getStaticPaths: GetStaticPaths = () => {
-  return {
-    paths: [],
-    fallback: 'blocking',
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getStaticProps: GetStaticProps<any, { slug: string }> = async ({
-  params,
-}) => {
-  const productSlug = params?.slug
-
-  const product = await stripe.products.retrieve(productSlug ?? '', {
-    expand: ['default_price'],
-  })
-
-  const price = product.default_price as Stripe.Price
-
-  return {
-    props: {
-      product: {
-        id: product.id,
-        name: product.name,
-        imageUrl: product.images[0],
-        description: product.description,
-        defaultPrice: new Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        }).format((price.unit_amount! * 1.5) / 100),
-        promotionPrice: new Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        }).format(price.unit_amount! / 100),
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        numberPrice: price.unit_amount! / 100,
-        defaultPriceId: price.id,
-      },
-    },
-    revalidate: 60 * 60 * 1, // 1 hour
-  }
 }
