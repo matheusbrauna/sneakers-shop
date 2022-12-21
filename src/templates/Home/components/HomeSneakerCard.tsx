ea25b92ac80d563ced21e53d225c438c852cff59
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-21 16:39:55 -0300

    feat: fix merge

--- src/templates/Home/components/HomeSneakerCard.tsx
+++ src/templates/Home/components/HomeSneakerCard.tsx
@@ -1,39 +1,52 @@
-import Image, { ImageProps } from 'next/image'
+import Image from 'next/image'
 import Link from 'next/link'
 
-interface HomeSneakerCardProps extends ImageProps {
-  title: string
-  category: string
-  price: string
+interface HomeSneakerCardProps {
+  sneaker: {
+    id: string
+    name: string
+    price: number
+    slug: string
+    image: {
+      url: string
+    }
+    category?: {
+      name: string
+    } | null
+  }
 }
 
-export function HomeSneakerCard({
-  category,
-  title,
-  price,
-  src,
-  alt,
-}: HomeSneakerCardProps) {
+export function HomeSneakerCard({ sneaker }: HomeSneakerCardProps) {
   return (
-    <Link href="/products/1" className="grid gap-5 place-content-center">
+    <Link
+      href={`/products/${sneaker.slug}`}
+      className="grid gap-5 keen-slider__slide place-content-center"
+    >
       <Image
-        src={src}
-        alt={alt}
+        src={sneaker.image.url}
+        alt={sneaker.name}
         width={373}
         height={373}
-        className="object-cover object-center"
+        quality={100}
       />
 
       <div className="flex justify-between">
         <div className="grid gap-2">
           <h2 className="text-base font-bold leading-5 text-neutral-900">
-            {title}
+            {sneaker.name}
           </h2>
 
-          <span className="text-xs leading-4 text-neutral-600">{category}</span>
+          <span className="text-xs leading-4 text-neutral-600">
+            {sneaker.category?.name}
+          </span>
         </div>
 
-        <p className="text-sm font-bold leading-5 text-neutral-900">{price}</p>
+        <p className="text-sm font-bold leading-5 text-neutral-900">
+          {new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(sneaker.price)}
+        </p>
       </div>
     </Link>
   )
--- src/templates/Home/containers/Banners.tsx
+++ src/templates/Home/containers/Banners.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 
-import bg1 from '../../../assets/bg1.png'
+import bg1 from '../../../assets/danilo-capece-NoVnXXmDNi0-unsplash.jpg'
 import { WhiteButton } from '../../../components/Buttons/WhiteButton'
 
 export function Banners() {
   return (
     <section className="py-8">
       <div className="container grid grid-cols-2">
-        <div className="grid text-center transition-opacity bg-blue-200 place-content-center hover:opacity-90">
-          <h2 className="text-3xl font-bold leading-9 text-neutral-50">
-            JIMMY X SHOP KOLEKSİYONU
+        <div className="grid text-center transition-opacity bg-red-400 place-content-center hover:opacity-90">
+          <h2 className="text-3xl font-bold leading-9 uppercase text-neutral-50">
+            Jordan 1 Low
           </h2>
 
           <p className="mt-3 text-sm leading-5 text-neutral-50">
@@ -28,6 +28,7 @@ export function Banners() {
           width={756}
           height={596}
           className="object-cover object-center"
+          quality={100}
         />
       </div>
     </section>
